d9fb6bfcc94d14ede8af3ae5a3fd093869a9a6c7
Oleg Kovalev <SinusPower@gmail.com>
2020-03-05 15:50:51 +0300

    Test commit

---
+++ test/hw01_hello_now/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
+const (
+	layout string = "2006-01-02 15:04:05 +0000 UTC" // time format
+	host   string = "0.beevik-ntp.pool.ntp.org"     // host for getting exact time
+)
+
+func main() {
+	now := time.Now()
+	fmt.Printf("current time: %s\n", now.Format(layout))
+	if exact, err := ntp.Time(host); err != nil {
+		fmt.Fprintf(os.Stderr, "error getting exact time: %v\n\t", err)
+		os.Exit(1)
+	} else {
+		fmt.Printf("exact time: %s\n", exact.Format(layout))
+	}
+}
