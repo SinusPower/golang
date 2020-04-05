86e3c0958436c073cb168fe71a5fc831f4e5a9d9
Oleg Kovalev <SinusPower@gmail.com>
2020-04-05 17:51:51 +0300

    Finished

--- test/hw04_lru_cache/cache.go
+++ test/hw04_lru_cache/cache.go
@@ -1,5 +1,7 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
+import "sync"
+
 type Key string
 
 type Cache interface {
@@ -18,6 +20,7 @@ type lruCache struct {
 	capacity int
 	queue    List
 	items    map[Key]*listItem
+	mutex    *sync.Mutex
 }
 
 func NewCache(capacity int) Cache {
@@ -25,14 +28,17 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*listItem),
+		mutex:    &sync.Mutex{},
 	}
 }
 
 func (lc *lruCache) Set(key Key, value interface{}) bool {
+	lc.mutex.Lock()
 	if itm, ok := lc.items[key]; ok { // refresh
 		refreshed := cacheItem{key, value}
 		itm.Value = refreshed
 		lc.queue.MoveToFront(lc.items[key])
+		lc.mutex.Unlock()
 		return true
 	}
 	// insert
@@ -42,20 +48,26 @@ func (lc *lruCache) Set(key Key, value interface{}) bool {
 		delete(lc.items, old.Value.(cacheItem).key)
 		lc.queue.Remove(old)
 	}
+	lc.mutex.Unlock()
 	return false
 }
 
 func (lc *lruCache) Get(key Key) (interface{}, bool) {
+	lc.mutex.Lock()
 	if itm, ok := lc.items[key]; ok { // return value
 		lc.queue.MoveToFront(lc.items[key])
-		return itm.Value, true
+		lc.mutex.Unlock()
+		return itm.Value.(cacheItem).value, true
 	}
+	lc.mutex.Unlock()
 	return nil, false
 }
 
 func (lc *lruCache) Clear() {
+	lc.mutex.Lock()
 	lc.queue = NewList()
 	lc.items = make(map[Key]*listItem)
+	lc.mutex.Unlock()
 }
 
 func (lc *lruCache) GetQueue() List {
